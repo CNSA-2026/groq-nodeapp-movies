a2e83829111d0565db6c31e0288e3e02a2e37f51
dsa069 <dsa069@inlumine.ual.es>
2026-04-07 15:17:03 +0000

    feat: implement MovieRecommendationsService and integrate with app

--- src/app.js
+++ src/app.js
@@ -1,14 +1,30 @@
-const express = require('express')
-const HelloWordService = require( "./services/hello-world" );
+const express = require('express');
+const HelloWordService = require('./services/hello-world');
+const bodyParser = require('body-parser');
 
-const app = express()
+const MovieRecommendationsService = require('./services/movie-recommendations');
+const configuration = require('./config/configuration');
+const { ChatGroq } = require('@langchain/groq');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.put('/post-test', async (req, res) => {
+  const recommendations = await new MovieRecommendationsService(
+    new ChatGroq({
+      apiKey: configuration.groq.apiKey,
+      model: 'deepseek-r1-distill-llama-70b',
+    })
+  ).getMovieRecommendations(req.body);
+  res.send(recommendations);
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.get('/:nameToSalute', (req, res) => {
   res.send(new HelloWordService().greet(req.params.nameToSalute));
-})
+});
 
-module.exports = app
+module.exports = app;
